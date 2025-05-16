587d8656cde31d4f32681e70db65a3205b7b67b5
Oksenss <127976024+Oksenss@users.noreply.github.com>
2025-05-16 08:39:46 +0000

    Whatever

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins don't waste time
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
